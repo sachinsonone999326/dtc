9f8746ff433d8537b5920ef609dd6fa99966fabf
sachinsonone999326 <sachin999326@gmail.com>
2026-03-03 17:54:27 +0530

    Add files via upload

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function index(){
+        
+        $user = Auth::user();
+        
+        
+    }
+}
